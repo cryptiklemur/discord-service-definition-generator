b9daa06c5147955381c85049e22fbb704dea0b06
Fonta <gianni@fonta.nl>
2019-05-06 20:46:10 +0200

    Added puppeteer flags for heroku

--- src/Parser.js
+++ src/Parser.js
@@ -76,7 +76,9 @@ export default class Parser {
             return cheerio.load(fs.readFileSync(file));
         } catch (error) {
             const url = `${baseUrl}/${type}/${resource}`;
-            const browser = await puppeteer.launch();
+            const browser = await puppeteer.launch({
+                args: ['--no-sandbox', '--disable-setuid-sandbox']
+            });
 
             const page = await browser.newPage();
             await page.goto(url);
